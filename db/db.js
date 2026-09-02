0134e4ca3a4fecefae411e2861657e5211fc5bf8
tsuki917 <einstrom917@gmail.com>
2026-09-02 18:40:01 +0900

    env対応 railway

--- db/db.js
+++ db/db.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-})
-
+    host: process.env.DB_HOST || process.env.MYSQLHOST,
+    port: process.env.DB_PORT || process.env.MYSQLPORT,
+    user: process.env.DB_USER || process.env.MYSQLUSER,
+    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
 module.exports = pool
