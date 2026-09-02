17f0c0b373f177886a9586a9902bf0800123b915
tsuki917 <einstrom917@gmail.com>
2026-09-02 19:21:45 +0900

    パラメータが欠けているときにエラー

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
 
 app.post("/recipes", async (req, res) => {
     const { title, making_time, serves, ingredients, cost } = req.body;
+    if (!title || !making_time || !serves || !ingredients || !cost) throw new Error("not found params")
     try {
         const [result] = await pool.query(
             'insert into recipes (title,making_time,serves,ingredients,cost) values (?,?,?,?,?)', [title, making_time, serves, ingredients, cost]
@@ -38,7 +39,7 @@ app.get("/recipes", async (req, res) => {
             'insert into recipes (title,making_time,serves,ingredients,cost) values (?,?,?,?,?)', [title, making_time, serves, ingredients, cost]
         )
         const [rows] = await pool.query('SELECT * FROM recipes WHERE id = ?', [result.insertId]);
-        res.status(200).json({ message: 'Recipe successfully created!', recipes: [rows] });
+        res.status(200).json({ recipes: [rows] });
     } catch (err) {
         res.status(500).json({
             "message": "Recipe creation failed!",
